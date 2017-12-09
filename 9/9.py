eb43944e579344825585d6c528c223fc9ab66d5a
past <poptart300@gmail.com>
2017-12-09 01:58:42 -0600

    day 9

---
+++ 9/9.py
@@ -0,0 +1,24 @@
+
+# AoC Day 9-1 & 9-2
+
+input = open("9.txt", "r").read()
+
+garbo = False
+skip = False
+sum1 = 0
+sum2 = 0
+lay = 0
+
+for char in input:
+	if (skip): skip = False; continue
+	if (not garbo):
+		if (char == "{"): lay += 1
+		elif (char == "}"): sum1 += lay; lay -= 1
+		elif(char == "<"): garbo = True
+	else:
+		if (char == "!"): skip = True
+		elif (char == ">"): garbo = False
+		else: sum2 += 1
+
+print("Group sum:", sum1)
+print("Garbo sum:", sum2)
